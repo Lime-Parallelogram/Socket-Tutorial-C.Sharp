e903724d4ffa7114a592fd63e2503a81bcc9b4d5
Lime Parallelogram <Lime-Parallelogram@users.noreply.github.com>
2017-08-21 17:09:40 +0100

    Update SocketServer.py

--- SocketServer.py
+++ SocketServer.py
@@ -1,27 +1,31 @@
+#Imports modules
 import socket
 import RPi.GPIO as GPIO
 import time
 
-listensocket = socket.socket()
-Port = 8000
+listensocket = socket.socket() #Creates an instance of socket
+Port = 8000 #Port to host server on
 maxConnections = 999
-IP = socket.gethostname()
+IP = socket.gethostname() #IP address of local machine
 
 listensocket.bind(('',Port))
 
+#Starts server
 listensocket.listen(maxConnections)
 print("Server started at " + IP + " on port " + str(Port))
 
+#Accepts the incomming connection
 (clientsocket, address) = listensocket.accept()
 print("New connection made!")
 
 running = True
 
+#Sets up the GPIOs --Can only be used on Raspberry Pi
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7,GPIO.OUT)
 
 while running:
-    message = clientsocket.recv(1024).decode()
+    message = clientsocket.recv(1024).decode() #Gets the incomming message
     print(message)
     if not message == "":
         GPIO.output(7,True)
