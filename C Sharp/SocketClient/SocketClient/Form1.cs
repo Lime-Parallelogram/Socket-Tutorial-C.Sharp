fd55c866e1f0ea2094a50ebca6cddb9a3a2ee0df
Lime Parallelogram <Lime-Parallelogram@users.noreply.github.com>
2018-03-31 20:19:26 +0100

    Update Form1.cs

--- C Sharp/SocketClient/SocketClient/Form1.cs
+++ C Sharp/SocketClient/SocketClient/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//Imports libraries
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -28,28 +29,28 @@ namespace SocketClient
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
+        //toolStripButton1 -- Open Connection
         private void toolStripButton1_Click(object sender, EventArgs e)
         {
             //Convert port number to text with error handling 
             if(!int.TryParse(textBox1.Text, out port))
             {
+                //Adds debug to list box and shows message box
                 MessageBox.Show("Port number not valied");
                 listBox1.Items.Add("Port number invalied");
             }
             
+            //Attemps to make connection
             try
             {
                 client = new TcpClient(textBox3.Text, port);
+                //Adds debug to list box and shows message box
                 MessageBox.Show("Connection Made");
                 listBox1.Items.Add("Made connection with " + textBox3.Text);
             }
             catch (System.Net.Sockets.SocketException)
             {
+                //Adds debug to list box and shows message box
                 MessageBox.Show("Connection Failed");
                 listBox1.Items.Add("Connection failed");
             }
@@ -57,30 +58,33 @@ namespace SocketClient
            
         }
 
+        //toolStripButton2 -- Send
         private void toolStripButton2_Click(object sender, EventArgs e)
         {
             try
             {
-                message = textBox2.Text;
-                byteCount = Encoding.ASCII.GetByteCount(message);
-                sendData = new byte[byteCount];
-                sendData = Encoding.ASCII.GetBytes(message);
-                stream = client.GetStream();
-                stream.Write(sendData, 0, sendData.Length);
-                listBox1.Items.Add("Sent Data " + message);
+                message = textBox2.Text; //Set message variable to input
+                byteCount = Encoding.ASCII.GetByteCount(message); //Measures bytes required to send ASCII data
+                sendData = new byte[byteCount]; //Prepares variable size for required data
+                sendData = Encoding.ASCII.GetBytes(message); //Sets the sendData variable to the actual binary data (from the ASCII)
+                stream = client.GetStream(); //Opens up the network stream
+                stream.Write(sendData, 0, sendData.Length); //Transmits data onto the stream
+                listBox1.Items.Add("Sent Data " + message); //Adds debug to the list box
             }
-            catch (System.NullReferenceException)
+            catch (System.NullReferenceException) //Error if socket not open
             {
+                //Adds debug to list box and shows message box
                 MessageBox.Show("Connection not installised");
                 listBox1.Items.Add("Failed to send data");
             }
         }
 
+        //toolStripButton3 -- Close Connection
         private void toolStripButton3_Click(object sender, EventArgs e)
         {
-            stream.Close();
-            client.Close();
-            listBox1.Items.Add("Connection terminated");
+            stream.Close(); //Closes data stream
+            client.Close(); //Closes socket
+            listBox1.Items.Add("Connection terminated"); //Adds debug message to list box
         }
     }
 }
