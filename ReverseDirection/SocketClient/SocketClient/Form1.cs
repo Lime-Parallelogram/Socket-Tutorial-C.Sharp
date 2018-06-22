c6bbe89a37ec7fda55d69b5af41ba54162031f2d
Lime Parallelogram <Lime-Parallelogram@users.noreply.github.com>
2018-06-22 17:20:18 +0100

    Update Form1.cs

--- ReverseDirection/SocketClient/SocketClient/Form1.cs
+++ ReverseDirection/SocketClient/SocketClient/Form1.cs
@@ -1,46 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+//Required Libs
+using System;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net;
 using System.Net.Sockets;
 
-namespace SocketClient
+namespace SocketClient //Sorry about the naming issue (This is a server)
 {
     public partial class Form1 : Form
     {
-        byte[] bytes = new byte[256];
+        //Vars
         TcpListener server;
         TcpClient clientconnection;
 
         public Form1()
         {
+            //Loads form components
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            server = new TcpListener(IPAddress.Any, 8000);
-            server.Start();
-            listBox1.Items.Add("Server Started");
-            clientconnection = server.AcceptTcpClient();
-            timer1.Start();
+            server = new TcpListener(IPAddress.Any, 8000); //Opens server port
+            server.Start(); //Starts server
+            listBox1.Items.Add("Server Started"); //Adds info to list
+            clientconnection = server.AcceptTcpClient(); //Accepts connection request
+            timer1.Start(); //Starts timer to check stream at regular intervals
         }
 
   
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            NetworkStream streamIn = clientconnection.GetStream();
-            byte[] buffer = new byte[clientconnection.ReceiveBufferSize];
-            int Data = streamIn.Read(buffer, 0, clientconnection.ReceiveBufferSize);
-            string ch = Encoding.ASCII.GetString(buffer, 0, Data);
-            listBox1.Items.Add(ch);
+            NetworkStream DataStream = clientconnection.GetStream(); //Gets the data stream
+            byte[] buffer = new byte[clientconnection.ReceiveBufferSize]; //Gets required buffer size
+            int Data = DataStream.Read(buffer, 0, clientconnection.ReceiveBufferSize); //Gets message (encoded)
+            string message = Encoding.ASCII.GetString(buffer, 0, Data); //Decoodes message
+            listBox1.Items.Add(message); //Adds message to output window
         }
     }
 }
