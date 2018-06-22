a24da93c96af8e16b21a2f94bd944b06b69b1a13
Lime Parallelogram <will@44ld.co.uk>
2018-06-22 16:47:42 +0100

    NewC#Files

---
+++ ReverseDirection/SocketClient/SocketClient/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace SocketClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "Output"});
+            this.listBox1.Location = new System.Drawing.Point(12, 17);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(459, 260);
+            this.listBox1.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 288);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Socket Server";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ReverseDirection/SocketClient/SocketClient/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketClient
+{
+    public partial class Form1 : Form
+    {
+        byte[] bytes = new byte[256];
+        TcpListener server;
+        TcpClient clientconnection;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            server = new TcpListener(IPAddress.Any, 8000);
+            server.Start();
+            listBox1.Items.Add("Server Started");
+            clientconnection = server.AcceptTcpClient();
+            timer1.Start();
+        }
+
+  
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            NetworkStream streamIn = clientconnection.GetStream();
+            byte[] buffer = new byte[clientconnection.ReceiveBufferSize];
+            int Data = streamIn.Read(buffer, 0, clientconnection.ReceiveBufferSize);
+            string ch = Encoding.ASCII.GetString(buffer, 0, Data);
+            listBox1.Items.Add(ch);
+        }
+    }
+}
+
+       
